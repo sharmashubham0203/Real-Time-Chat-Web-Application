1dca7d8a9ce7cce9ddfc1272a6113d7a6b03bec0
Shubham Sharma <138666350+sharmashubham0203@users.noreply.github.com>
2025-01-22 20:58:57 +0530

    Update admin.ts

--- my-project/config/admin.ts
+++ my-project/config/admin.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'defaultSecret'),
+    secret: env('ADMIN_JWT_SECRET', 'your-generated-secret'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
