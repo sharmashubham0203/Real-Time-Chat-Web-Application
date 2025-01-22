db2f48c2b2dd73d0d4d0f75a7e1991dfbde7eabe
Shubham Sharma <138666350+sharmashubham0203@users.noreply.github.com>
2025-01-22 22:24:16 +0530

    Update middlewares.ts

--- my-project/config/middlewares.ts
+++ my-project/config/middlewares.ts
@@ -1,12 +1,32 @@
-export default [
-  'strapi::logger',
+// export default [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
+
+module.exports = [
   'strapi::errors',
   'strapi::security',
   'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
 ];
+
+// Update the CORS middleware to allow requests from your frontend
+module.exports = {
+  settings: {
+    cors: {
+      origin: ['https://real-time-chat-web-application-2.onrender.com'],
+    },
+  },
+};
