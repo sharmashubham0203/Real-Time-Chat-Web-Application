d14f1bdbaf304baef58811bc135934b4230d02b3
Shubham Sharma <138666350+sharmashubham0203@users.noreply.github.com>
2025-01-22 20:22:31 +0530

    Update server.ts

--- my-project/config/server.ts
+++ my-project/config/server.ts
@@ -1,7 +1,16 @@
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
+
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
   },
 });
+
